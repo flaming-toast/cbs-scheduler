d31a134029d41396607a60788920644ef6193b0f
Matthew Leung <leung.mattp@berkeley.edu>
2014-02-09 21:43:48 -0500

    [test_mm.c] indentation [mm_alloc] begin linked list impl

--- httpd/mm_alloc.c
+++ httpd/mm_alloc.c
@@ -8,33 +8,119 @@
 #include "mm_alloc.h"
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 /* Your final implementation should comment out this macro. */
 #define MM_USE_STUBS
 
-void* mm_malloc(size_t size)
+void *mm_malloc(size_t size)
+{
+//        return calloc(1, size);
+        return mm_malloc_ll(size);
+}
+
+void *mm_realloc(void *ptr, size_t size)
 {
 #ifdef MM_USE_STUBS
-	return calloc(1, size);
+        return realloc(ptr, size);
 #else
-#error Not implemented.
+        return mm_realloc_ll(ptr, size);
 #endif
 }
 
-void* mm_realloc(void* ptr, size_t size)
+void mm_free(void *ptr)
 {
 #ifdef MM_USE_STUBS
-	return realloc(ptr, size);
+        free(ptr);
 #else
-#error Not implemented.
+        mm_free_ll(ptr);
 #endif
+}
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////// STANDARD LINKED LIST IMPLEMENTATION /////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+#define FREE 1
+#define USED 0
+#define TRUE 1
+#define FALSE 0
+#define ERROR -1
+#define SUCCESS 1
+
+const size_t  INIT_MEM_SIZE     = 8192;
+const int     NODE_HEADER_SIZE  = 32;
+
+MM_node *mm_head = NULL;
+
+MM_node *construct_node(void *addr, MM_node *prev)
+{
+        MM_node *node = addr;
+        node->free_size = 0;
+        node->next_free = NULL;
+        node->prev = prev;
+        node->status = FREE;
+        return node;
 }
 
-void mm_free(void* ptr)
+int initialize(size_t req_mem_size)
 {
-#ifdef MM_USE_STUBS
-	free(ptr);
-#else
-#error Not implemented.
-#endif
+        void *heap_bottom;
+
+        req_mem_size = get_mem_size(req_mem_size);
+
+        if((heap_bottom = sbrk(req_mem_size)) == NULL)
+        {
+                return ERROR;
+        } else {
+                heap_bottom = align_addr(heap_bottom);
+                mm_head = construct_node(heap_bottom - req_mem_size, NULL);
+                printf("heap bottom: %p, %p,  %li\n", heap_bottom, mm_head, (long)heap_bottom % 8);
+                printf("top mm_head: %d, %p, %p, %d\n", mm_head->free_size, mm_head->next_free, mm_head->prev, mm_head->status);
+
+                return SUCCESS;
+        }
 }
+/*
+ * returns the closest address that is a multiple of 8 to the provided address.
+ */
+void *align_addr(void *addr)
+{
+        if((long)addr % 8 != 0)
+        {
+                // TODO
+                return addr;
+        } else {
+                return addr;
+        }
+}
+
+int get_mem_size(size_t req_mem_size)
+{
+        // we want the inital sbrk to be a multiple of 8K for uniformity
+        return INIT_MEM_SIZE * (req_mem_size / INIT_MEM_SIZE + 1);
+}
+
+void *mm_malloc_ll(size_t size)
+{
+        int status;
+
+        if(mm_head == NULL)
+        {
+                status = initialize(size);
+        }
+        if(status == ERROR)
+                return NULL;
+
+        return calloc(1, size);
+}
+
+void *mm_realloc_ll(void *ptr, size_t size)
+{
+        return realloc(ptr, size);
+}
+
+void mm_free_ll(void *ptr)
+{
+        free(ptr);
+}
+
--- httpd/mm_alloc.h
+++ httpd/mm_alloc.h
@@ -12,9 +12,33 @@ extern "C" {
 
 #include <stdlib.h>
 
-void* mm_malloc(size_t size);
-void* mm_realloc(void* ptr, size_t size);
-void mm_free(void* ptr);
+void *mm_malloc(size_t size);
+void *mm_realloc(void *ptr, size_t size);
+void mm_free(void *ptr);
+
+/*
+ * mm_malloc node in memory list
+ */
+typedef struct mm_node
+{
+// size of this block's free space (not counting the header)
+        int free_size;
+// pointer to next free block
+        struct mm_node *next_free;
+// pointer to immediately previous block
+        struct mm_node *prev;
+// information regarding if this block is free or taken
+        int status;
+} MM_node;
+
+MM_node *construct_node(void *addr, MM_node *prev);
+int initialize(size_t mem_init_size);
+void *mm_malloc_ll(size_t size);
+void *mm_realloc_ll(void *ptr, size_t size);
+void mm_free_ll(void *ptr);
+int get_mem_size(size_t req_mem_size);
+void *align_addr(void *addr);
+
 
 #ifdef __cplusplus
 }
