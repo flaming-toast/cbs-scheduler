d549392e27a4cdafcaf45a7c82f1bb07b0090aab
Patrick Dale-Verlin Harvey <p1harvey@berkeley.edu>
2014-02-13 13:08:25 -0500

    <httpd/test_prctl.c> Finished the test file, although make isn't working with it <linux/kernel/fork.c> Removed incorrect references to atomic_t's

--- httpd/test_prctl.c
+++ httpd/test_prctl.c
@@ -1,5 +1,14 @@
+#ifdef PRCTL_TEST
 #include "Cunit/Basic.h"
 #include <sys/prctl.h>
+
+static int nop_init_suite(void) {
+  return 0;
+}
+
+static int nop_clean_suite(void) {
+  return 0;
+}
 
 static void test_prctl_stop_fork(void) {
   prctl(PR_SET_THREADLIMIT, 0);
@@ -18,7 +27,33 @@ static void test_prctl_stop_clone(void) {
 static void test_prctl_reenable(void) {
   prctl(PR_SET_THREADLIMIT, 0);
   prctl(PR_SET_THREADLIMIT, 2);
-  if (fork()) {
-    
+  unsigned long npid = fork();
+  if (npid == 0) {
+    exit();
   }
+  CU_ASSERT(npid != 0)
 }
+
+
+int main(int argc, char **argv)
+{
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    {
+        CU_pSuite prctlSuite = CU_add_suite("prctl tests",
+                                   &nop_init_suite,
+                                   &nop_clean_suite);
+
+        CU_add_test(prctlSuite, "Test of fork() disabling with prctl()", &test_prctl_stop_fork);
+	CU_add_test(prctlSuite, "Test of clone() disabling with prctl()", &test_prctl_stop_clone);
+	CU_add_test(prctlSuite, "Test of fork() reenabling after disabling", &test_prctl_reenable);
+    }
+
+    /* Actually run your tests here. */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
+#endif
