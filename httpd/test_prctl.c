7770fd6a6051a8e1794ec6a157050ca5e835c42a
Patrick Dale-Verlin Harvey <p1harvey@berkeley.edu>
2014-02-20 12:34:08 -0500

    <httpd/test_prctl.c> Some debugging of test routines

--- httpd/test_prctl.c
+++ httpd/test_prctl.c
@@ -1,8 +1,19 @@
 #ifdef PRCTL_TEST
-#include "Cunit/Basic.h"
+#include "CUnit/Basic.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/prctl.h>
+#include <sched.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#define SET_TL_DUMMY 41
+#define GET_TL_DUMMY 42
 
 int startFlag = 0;
+int BASIC_STACK_SIZE = 500;
+
 
 static int nop_init_suite(void) {
   return 0;
@@ -12,93 +23,114 @@ static int nop_clean_suite(void) {
   return 0;
 }
 
+/* Utility functions used in test routines */
+
+int wait_for_start_and_exit(void* ign) {
+  while(!startFlag) { 
+    /* spin, we don't really care */
+  }
+  return 0;
+}
+
+int fork_new_and_ret_pid() {
+  int npid = fork();
+  if (npid == 0) {
+    exit(0);
+    return 0;
+  }
+  return npid;
+}
+
+int clone_new_dummy_fn(void* fn) {
+  return 0;
+}
+
+int clone_new_and_ret_pid(void* fn) {
+  void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
+  if (c_stack == NULL) {
+    CU_ASSERT(1 == 0);
+    return 0;
+  }
+  int npid = clone(clone_new_dummy_fn, c_stack, SIGCHLD, NULL);
+  int ign = 0;
+  if (npid > 0) {
+    wait(&ign);
+  }
+  free(c_stack);
+  return npid;
+}
+
+int clone_new_and_ret_result(void* fn) {
+  void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
+  if (c_stack == NULL) {
+    CU_ASSERT(1 == 0);
+    return 0;
+  }
+  int npid = clone((int (*fn)(void *))fn, c_stack, SIGCHLD, NULL);
+  int res = 0;
+  if (npid > 0) {
+    wait(&res);
+  }
+  free(c_stack);
+  return res;
+}
+
+/* Test routines */
+
+int run_test_prctl_limit1(void* ign) {
+  prctl(SET_TL_DUMMY, 1);
+  int npid = fork();
+  if (npid == 0) {
+    exit(0);
+    return npid;
+  }
+  return npid;
+}
+
 static void test_prctl_limit1_fail(void) {
   void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
   if (c_stack == NULL) {
-    CUASSERT(1 == 0);
+    CU_ASSERT(1 == 0);
     return;
   }
   int npid = clone(run_test_prctl_limit1, c_stack, SIGCHLD, NULL);
   int res = 0;
-  CUASSERT(npid > 0);
+  CU_ASSERT(npid > 0);
   if (npid > 0) {
     wait(&res);
-    CUASSERT(res == -1);
+    CU_ASSERT(res == -1);
   }   
   free(c_stack);
+}
+
+int run_test_prctl_limit2_success(void* ign) {
+  prctl(SET_TL_DUMMY, 2);
+  int npid = fork();
+  if (npid == 0) {
+    exit(0);
+    return npid;
+  }
+  return npid;
 }
 
 static void test_prctl_limit2_success(void) {
   void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
   if (c_stack == NULL) {
-    CUASSERT(1 == 0);
+    CU_ASSERT(1 == 0);
     return;
   }
   int npid = clone(run_test_prctl_limit2_success, c_stack, SIGCHLD, NULL);
   int res = 0;
-  CUASSERT(npid > 0);
+  CU_ASSERT(npid > 0);
   if (npid > 0) {
     wait(&res);
-    CUASSERT(res > 0);
+    CU_ASSERT(res > 0);
   }
   free(c_stack);
-}
-
-static void test_prctl_limit2_failch(void) {
-  void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
-  if (c_stack == NULL) {
-    CUASSERT(1 == 0);
-    return;
-  }
-  int npid = clone(run_test_prctl_limit2_failch, c_stack, SIGCHLD, NULL);
-  int res = 0;
-  CUASSERT(npid > 0);
-  if (npid > 0) {
-    wait(&res);
-    CUASSERT(res == -1);
-  }
-  free(c_stack);
-}
-
-static void test_prctl_limit2_failsi(void) {
-  void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
-  if (c_stack == NULL) {
-    CUASSERT(1 == 0);
-    return;
-  }
-  int npid = clone(run_test_prctl_limit2_failsi, c_stack, SIGCHLD, NULL);
-  int res = 0;
-  CUASSERT(npid > 0);
-  if (npid > 0) {
-    wait(&res);
-    CUASSERT(res < 0); /* The first task was created successfully */
-    CUASSERT(res == -2); /* The second task was not created */
-  }
-  free(c_stack);
-}
-
-int run_test_prctl_limit1(void* ign) {
-  prctl(PR_SET_THREADLIMIT, 1);
-  int npid = fork();
-  if (npid == 0) {
-    exit();
-    return npid;
-  }
-  return npid;
-}
-
-int run_test_prctl_limit2_success(void* ign) {
-  prctl(PR_SET_THREADLIMIT, 2);
-  int npid = fork();
-  if (npid == 0) {
-    exit();
-    return npid;
-  }
-  return npid;
 }
 
 int run_test_prctl_limit2_failch(void* ign) {
-  prctl(PR_SET_THREADLIMIT, 2);
+  prctl(SET_TL_DUMMY, 2);
   void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
   if (c_stack == NULL) {
     CUASSERT(1 == 0);
@@ -113,8 +145,24 @@ int run_test_prctl_limit2_failch(void* ign) {
   return res;
 }
 
+static void test_prctl_limit2_failch(void) {
+  void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
+  if (c_stack == NULL) {
+    CU_ASSERT(1 == 0);
+    return;
+  }
+  int npid = clone(run_test_prctl_limit2_failch, c_stack, SIGCHLD, NULL);
+  int res = 0;
+  CU_ASSERT(npid > 0);
+  if (npid > 0) {
+    wait(&res);
+    CU_ASSERT(res == -1);
+  }
+  free(c_stack);
+}
+
 int run_test_prctl_limit2_failsi(void* ign) {
-  prctl(PR_SET_THREADLIMIT, 2);
+  prctl(SET_TL_DUMMY, 2);
   void* c_stack1 = malloc(BASIC_STACK_SIZE*sizeof(int));
   if (c_stack1 == NULL) {
     CUASSERT(1 == 0);
@@ -145,56 +193,22 @@ int run_test_prctl_limit2_failsi(void* ign) {
   return rval;
 }
 
-int wait_for_start_and_exit(void* ign) {
-  while(!startFlag) { 
-    /* spin, we don't really care */
-  }
-  return 0;
-}
-
-int clone_new_and_ret_result(void* fn) {
+static void test_prctl_limit2_failsi(void) {
   void* c_stack = malloc(BASIC_STACK_SIZE*sizeof(int));
   if (c_stack == NULL) {
-    CUASSERT(1 == 0);
+    CU_ASSERT(1 == 0);
     return;
   }
-  int npid = clone((int (*fn)(void *))fn, c_stack, SIGCHLD, NULL);
+  int npid = clone(run_test_prctl_limit2_failsi, c_stack, SIGCHLD, NULL);
   int res = 0;
+  CU_ASSERT(npid > 0);
   if (npid > 0) {
     wait(&res);
+    CU_ASSERT(res < 0); /* The first task was created successfully */
+    CU_ASSERT(res == -2); /* The second task was not created */
   }
   free(c_stack);
-  return res;
 }
-
-int fork_new_and_ret_pid() {
-  int npid = fork();
-  if (npid == 0) {
-    exit();
-    return 0;
-  }
-  return npid;
-}
-
-int clone_new_and_ret_pid(void* fn) {
-  void* c_stack = malloc(BASIC_STACK_SIZE*sieof(int));
-  if (c_stack == NULL) {
-    CUASSERT(1 == 0);
-    return;
-  }
-  int npid = clone(clone_new_dummy_fn, c_stack, SIGCHLD, NULL);
-  int ign = 0;
-  if (npid > 0) {
-    wait(&ign);
-  }
-  free(c_stack);
-  return npid;
-}
-
-int clone_new_dummy_fn(void* fn) {
-  return 0;
-}
-
 
 int main(int argc, char **argv)
 {
@@ -206,9 +220,10 @@ int main(int argc, char **argv)
                                    &nop_init_suite,
                                    &nop_clean_suite);
 
-        CU_add_test(prctlSuite, "Test of fork() disabling with prctl()", &test_prctl_limit1_fail);
-	CU_add_test(prctlSuite, "Test of clone() disabling with prctl()", &test_prctl_limit2_success);
-	CU_add_test(prctlSuite, "Test of fork() reenabling after disabling", &test_prctl_limit2_failch);
+        CU_add_test(prctlSuite, "Test of prctl(1) disabling new thread formation", &test_prctl_limit1_fail);
+	CU_add_test(prctlSuite, "Test of prctl(2) allowing a child thread to be formed", &test_prctl_limit2_success);
+	CU_add_test(prctlSuite, "Test of prctl(2) preventing child thread from forming another child", &test_prctl_limit2_failch);
+	CU_add_test(prctlSuite, "Test of prctl(2) preventing a second child of the original thread from being formed", &test_prctl_limit2_failsi);
     }
 
     /* Actually run your tests here. */
