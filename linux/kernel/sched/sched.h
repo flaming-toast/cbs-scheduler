88c80b32423232f3dc53cb7d806e1eaace1deb2e
Jessica Yu <flamingtoast@berkeley.edu>
2014-04-13 19:23:13 -0400

    <kernel/sched/cbs.c,sched.h> - Switch to 24.8 fp arithmetic for utilization calculations

--- linux/kernel/sched/sched.h
+++ linux/kernel/sched/sched.h
@@ -340,8 +340,8 @@ struct cbs_rq {
 
 	struct sched_cbs_entity *curr;
 
-	unsigned long total_sched_cbs_budget;
-	unsigned long total_sched_cbs_period;
+	u64 total_sched_cbs_utilization; /* 24.8 fp value */
+	u64 total_sched_cbs_periods; /* Not sure if it makes sense to keep track of this */
 };
 
 extern void insert_cbs_rq(struct cbs_rq *rq, struct sched_cbs_entity *se, int rebalance);
